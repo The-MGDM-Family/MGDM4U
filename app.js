c097c07c0c63f7f251ac044bbac020faa39ace03
The-MGDM-Family <110233248+The-MGDM-Family@users.noreply.github.com>
2022-12-27 13:44:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const {google} = require('googleapis');
+const { auth } = require("googleapis/build/src/apis/abusiveexperiencereport");
+const googleSheets = google.sheets({version: 'v4', });
 
 const app = express();
 
@@ -14,6 +17,8 @@ let shopItems = [{
   email: "mugi.gunalan@gmail.com",
   cost:"₹200"
 }];
+
+let stories = [{ name: "the-mistake", likes: 0 }];
 
 app.set('view engine', 'ejs');
 
@@ -54,10 +59,75 @@ app.get("/shopping-:itemName", function(req, res){
   });
 });
 
-app.get("/novel-the-mistake", function(req, res){
-  res.render("the-mistake");
+app.get("/latest-update", function(req, res){
+  res.render("update");
 });
 
-app.listen(process.env.PORT || 3000, function(){
+// app.get("/subscribe", function(req, res){
+//   res.render("subscribe");
+// });
+//
+// app.post("/", async function (req, res) {
+//   const data = {
+//     name: req.body.name,
+//     email: req.body.email
+//   }
+//
+//   const auth = new google.auth.GoogleAuth({
+//     keyFile: "credentials.json",
+//     scopes: "https://www.googleapis.com/auth/spreadsheets"
+//   });
+//
+//   const client = await auth.getClient();
+//   const spreadsheetId = "1j13wEpFAQvtnQS7VQNzVWC0KExaUj1xWVwqrwCGbVOM";
+//
+//   const metaData = googleSheets.get({
+//     auth,
+//     spreadsheetId
+//   });
+//
+//   const getRows = await googleSheets.spreadsheets.values.get({
+//     auth,
+//     spreadsheetId,
+//     range: "Sheet1!A2:B"
+//   });
+//
+// async function main () {
+//   const authClient = await authorize();
+//   const request = {
+//     // The ID of the spreadsheet to update.
+//     spreadsheetId: 'my-spreadsheet-id',  // TODO: Update placeholder value.
+//     resource: {
+//       // How the input data should be interpreted.
+//       valueInputOption: '',
+//       data: [data],
+//     },
+//     auth: authClient,
+//   };
+//  }
+// });
+
+app.get("/the-mistake", function(req, res){
+  res.render("the-mistake", {
+    likes: stories[0].likes
+  });
+});
+
+app.post("/:storyNo/like", function(req, res){
+  let i = req.params.storyNo;
+  stories[i].likes = stories[i].likes + 1;
+  res.redirect("/novel-" + stories[i].name);
+});
+
+app.post("/:storyNo/dislike", function(req, res){
+  let i = req.params.storyNo;
+  stories[i].likes = stories[i].likes - 1;
+  res.redirect("/novel-" + stories[i].name);
+});
+
+app.listen(3000 || process.env.PORT, function(){
   console.log("Server runing on port 3000.");
 });
+
+// 880558919012-ulfchtfleqm47c7tds4f6uv0qm7f6e20.apps.googleusercontent.com - Client Id
+// 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 - client secret
